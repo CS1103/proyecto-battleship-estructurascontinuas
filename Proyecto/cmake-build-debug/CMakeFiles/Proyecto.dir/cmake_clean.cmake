3904172fcc3da7afad4ce66ba5d4d02ea5600d35
lender <luis.berrospi@utec.edu.pe>
2020-06-15 00:27:31 -0500

    main

---
+++ Proyecto/cmake-build-debug/CMakeFiles/Proyecto.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proyecto.dir/main.cpp.o"
+  "Proyecto.exe"
+  "Proyecto.pdb"
+  "libProyecto.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proyecto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Proyecto/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Mario wn!" << std::endl;
+    return 0;
+}
