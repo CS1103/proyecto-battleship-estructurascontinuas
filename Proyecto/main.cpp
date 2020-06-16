d36312e77e389812024e824619b0ceccf4ea02ac
lender <luis.berrospi@utec.edu.pe>
2020-06-16 18:54:52 -0500

    funcion in, leer .out

--- Proyecto/main.cpp
+++ Proyecto/main.cpp
@@ -9,6 +9,23 @@ void out(const string& str) {
     file << str;
 }
 
+string in () {
+    ifstream file;
+    string comando;
+    string line;
+    file.open("../out/FirstPlayer.out");
+    if (file.is_open    ())
+    {
+        while ( getline (file,line) )
+        {
+            comando += line + '\n';
+        }
+        file.close();
+    }
+    else{cout << "no se puede abrir";}
+    return line;
+}
+
 void handshake(const string& name) {
     string str = "HANDSHAKE=" + name;
     out(str);
@@ -16,4 +33,5 @@ void handshake(const string& name) {
 
 int main() {
     handshake("dinamita team");
+    cout << in();
 }
