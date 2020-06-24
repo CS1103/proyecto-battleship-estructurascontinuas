30345c1dc21605ff6e6625610dcdfd3d490cc07d
lender <luis.berrospi@utec.edu.pe>
2020-06-24 17:40:15 -0500

    fix

---
+++ Proyect/main.cpp
@@ -0,0 +1,24 @@
+#include "common_types.h"
+
+#include "navy_t.h"
+#include "controller_t.h"
+
+using namespace std;
+
+int main() {
+    //handshakeIn("dinamita team");
+    //statement_t st = waitIn();
+    //statement_t* pst = &st;
+    //cout << st.token << endl;
+    //string type = "A";
+    //out(placeFleet(pst, type));
+    //board_t b = board_t('J', 10);
+    //navy_t n1 = navy_t(2,3,'A', 'H', b);
+    //navy_t n2 = navy_t(4,6,'S', 'V', b);
+    //navy_t n3 = navy_t(1,8,'B', 'H', b);
+    //b.print();
+    string name = "BattleBot";
+    controller_t ctr(name);
+    ctr.execute();
+    return 0;
+}
